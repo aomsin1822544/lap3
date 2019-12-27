55a8f53ba0ee117bcc3e48932044086e98b766ab
aomsin1822544 <pspsas333_111@hotmail.com>
2019-12-27 18:19:49 +0700

    v1

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
 using namespace std;
-
 int main()
 {
-    int n<70
- while(1/n+1){
-     cout << 1/n+1 <<
+
+    float n=2;
+    float total=0;
+     while(n<=69){
+     total=total+(1/n);
+     n=n+1;
+          
  }
+ cout << total;
   
+  return 0;
 
 }
